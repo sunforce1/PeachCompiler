0870ca6eb65666d9446f784cb77e82c9fd72f909
sunforce12 <stefan.m.soare@gmail.com>
2023-11-28 12:48:13 +0200

    frist lexer preparation

--- compiler.c
+++ compiler.c
@@ -1,4 +1,11 @@
 #include "compiler.h"
+
+struct lex_process_functions compiler_lex_functions = {
+    .next_char = compile_process_next_char,
+    .peek_char = compile_process_peek_char,
+    .push_char = compile_process_push_char
+ 
+};
 int compile_file(const char* filename, const char* out_filename, int flags)
 {
     struct compile_process* process = compile_process_create(filename, out_filename, flags);
@@ -8,6 +15,15 @@ int compile_file(const char* filename, const char* out_filename, int flags)
     }
 
     //perform lexical analysys
+    struct lex_process* lex_process = lex_process_create(process, &compiler_lex_functions, NULL);
+
+    if (!lex_process) {
+        return COMPILER_FAILED_WITH_ERRORS;
+    }
+
+    if(lex(lex_process) != LEXICAL_ANALYIS_ALL_OK) {
+        return LEXICAL_ANALYSIS_INPUT_ERROR;
+    }
 
     //perform parsing
 
--- compiler.h
+++ compiler.h
@@ -1,6 +1,76 @@
 #ifndef PEACHCOMPILER_H
 #define PEACHCOMPILER_H
 #include <stdio.h>
+#include <stdbool.h>
+
+struct pos {
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum {
+    LEXICAL_ANALYIS_ALL_OK,
+    LEXICAL_ANALYSIS_INPUT_ERROR
+};
+
+enum {
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+
+struct token {
+    int type;
+    int flags;
+
+    union {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+
+    // it is true if thre is whitespace between token and next token
+    bool whitespace;
+
+    // points to first char address of something in the brackets
+    const char* between_brackets;
+};
+
+struct lex_process;
+typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process* process);
+typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process* process);
+typedef void (*LEX_PROCESS_PUSH_CHAR)(struct lex_process* process, char c);
+
+struct lex_process_functions {
+    LEX_PROCESS_NEXT_CHAR next_char;
+    LEX_PROCESS_PEEK_CHAR peek_char;
+    LEX_PROCESS_PUSH_CHAR push_char;
+};
+
+
+struct lex_process {
+    struct pos pos;
+    struct vector* token_vec;
+    struct compile_process* compiler;
+
+    int current_expression_count;
+    struct buffer* parentheses_buffer;
+    struct lex_process_functions*  function;
+
+    void* private;
+
+
+};
 
 enum {
     COMPILER_FILE_COMPILED_OK,
@@ -9,7 +79,7 @@ enum {
 
 struct compile_process {
     int flags;
-
+    struct pos pos;
     struct compile_process_input_file
     {
         FILE* fp;
@@ -24,5 +94,13 @@ struct compile_process {
 
 int compile_file(const char* filename, const char* out_filename, int flags);
 struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags);
+char compile_process_next_char(struct lex_process* lex_process);
+char compile_process_peek_char(struct lex_process* lex_process);
+void compile_process_push_char(struct lex_process* lex_process, char c);
 
+struct lex_process* lex_process_create(struct compile_process* compiler, struct lex_process_functions* functions, void* private);
+void lex_process_free(struct lex_process* process);
+void* lex_process_private(struct lex_process* process);
+struct vector* lex_process_tokens(struct lex_process* process);
+int lex(struct lex_process* process);
 #endif
